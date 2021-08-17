46606a6758363a99d6b085840985be0cf54e73a4
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2021-08-17 17:41:20 +0200

    pull-request from #11

--- mlp.py
+++ mlp.py
@@ -34,9 +34,12 @@ from __future__ import print_function, division
 import numpy as np
 from numpy import sum, pi, cos, sin, arctan2, exp, log, sqrt,\
                   dot, argmax, arange
-from pause import *
-from gplot import *
+#from pause import *
+#from gplot import *
 from scipy import optimize as op
+import time 
+from pathos.pools import ProcessPool as Pool
+
 
 # for python3: emulate the nice python2 behaviour of map and zip
 xmap = map
@@ -64,17 +67,13 @@ def mod_c(x, a):
 class Gls:
     """
     Compute the Generalized Lomb-Scargle (GLS) periodogram.
-
     The *Gls* class computes the error-weighted Lomb-Scargle periodogram as
     developed by [ZK09]_ using various possible normalizations.
-
     The constructor of *Gls* takes a *TimeSeries* instance (i.e., a light curve)
     as first argument. The constructor allows to pass keywords to adjust the
     `freq` array, which will be used to calculate the periodogram.
-
     The main result of the calculation, i.e., the power, are stored in the
     class property `power`.
-
     Parameters
     ----------
     lc : TimeSeries object or tuple or list
@@ -108,7 +107,6 @@ class Gls:
         leading to faster evaluation (default is False).
     verbose : boolean, optional
         Set True to obtain some statistical output (default is False).
-
     Attributes
     ----------
     power : array
@@ -127,34 +125,31 @@ class Gls:
         The errors of the data values.
     norm : string, {'lnL', 'Scargle', 'HorneBaliunas', 'Cumming', 'wrms', 'chisq'}
         The used normalization.
-
     Examples
     --------
     Create 1000 unevenly sampled data points with frequency=0.1,
     measurement error and Gaussian noise
     >>> time = np.random.uniform(54000., 56000., 1000)
     >>> flux = 0.15 * np.sin(2. * np.pi * time / 10.)
-
     Add some noise
     >>> error = 0.3 * np.ones(time.size)
     >>> flux += np.random.normal(0, error+0.2)
-
     Compute the full error-weighted Lomb-Periodogram
     in 'lnL' normalization and calculate the significance
     of the maximum peak.
     >>> gls = Gls((time, flux, error), verbose=True)
-
     >>> maxPower = gls.pmax
     >>> print("GLS maximum power: ", maxPower)
     >>> print("GLS statistics of maximum power peak: ", gls.stats(maxPower))
     >>> gls.plot(block=True)
-
     """
     # Available normalizations
     norms = ['dlnL', 'lnL', 'Scargle', 'HorneBaliunas', 'Cumming', 'wrms', 'chisq']
 
-    def __init__(self, lc, fbeg=None, fend=None, Pbeg=None, Pend=None, ofac=10, hifac=1, freq=None, norm="dlnL", ls=False, fast=False, verbose=False, **kwargs):
+    def __init__(self, lc, fbeg=None, fend=None, Pbeg=None, Pend=None, ofac=10, hifac=1, freq=None, norm="dlnL", ls=False, ncpus = 1, fast=False, verbose=False, **kwargs):
 
+
+        self.ncpus = ncpus 
         self.freq = freq
         self.fbeg = fbeg
         self.fend = fend
@@ -186,7 +181,6 @@ class Gls:
     def _assignTimeSeries(self, lcs):
       """
       A container class that holds the observed light curve.
-
       Parameters
       ----------
       time : array
@@ -195,7 +189,6 @@ class Gls:
           The observed flux/data.
       error : array, optional
           The error of the data values.
-
       """
       self.t = []
       self.y = []
@@ -239,12 +232,10 @@ class Gls:
     def _buildFreq(self):
         """
         Build frequency array (`freq` attribute).
-
         Attributes
         ----------
         fnyq : float
             Half of the average sampling frequency of the time series.
-
         """
         self.fstep = 1 / self.tbase / self.ofac   # frequency sampling depends on the time span, default for start frequency
         self.fnyq = 0.5 / self.tbase * self.N     # Nyquist frequency
@@ -287,6 +278,29 @@ class Gls:
           wtrms += [np.sqrt(np.sum((y-ymod)**2/sigma2)/np.sum(1/sigma2))]
        return sum(L)
 
+
+    def partial_func(self,omega):
+
+      # Circular frequencies
+      X = [omega*th for th in self.th]
+      cosX = map(cos, X)
+      sinX = map(sin, X)
+      a = op.fmin_powell(self.nll, self.a, args=(zip(cosX,sinX), self.y, self.e_y, mod_abc), disp=False)
+
+      _a= a[0]
+      _b= a[1]
+      _off= a[2:2+self.Nj]
+      _lnMLj = L
+      _chisqr = chisqr
+      lnML = sum(L)
+      _wtrms = wtrms
+      W = [np.sum(1/(e_y**2+jit**2)) for e_y,jit in zip(self.e_y, a[-self.Nj:])]
+      _wrms = np.sqrt(np.sum(chisqr)/np.sum(W))   # a bit handwavy defined
+      
+      return [a, _a,_b,_off,_lnMLj,_chisqr,lnML,_wtrms,_wrms]
+
+
+
     def _calcPeriodogram(self):
 
         self._a, self._b, self.p, self.lnML = np.zeros((4, self.nf))
@@ -304,8 +318,8 @@ class Gls:
         nll = lambda *args: -self.lnL(*args)
         #a0 = [0.]*self.Nj + [3.]*self.Nj # start guess for first frequency
         a0 = map(np.mean, self.y) + map(np.std, self.y)
-        print(a0)
-
+        #print(a0)
+        self.nll = nll
         # The model with only offset c
         a0 = op.fmin_powell(nll, a0, args=(self.th, self.y, self.e_y, mod_c), disp=False)
         self.lnML0 = sum(L)
@@ -316,40 +330,37 @@ class Gls:
 
         self.chisqr = chisqr
         self.wtrms = wtrms
-        print(sum(L), L, a0)
-        a = [0., 0.] + list(a0)   # start guess for first frequency
-        a = [np.median(a0[len(a0)//2:])]*2 + list(a0)   # start guess for first frequency
-        for k, omega in enumerate(2.*pi*self.freq):
-          # Circular frequencies
-          X = [omega*th for th in self.th]
-          cosX = map(cos, X)
-          sinX = map(sin, X)
-          # self.lnL(theta, zip(cosX,sinX), self.y, self.e_y, mod_abc)
-          #a = op.fmin(nll, a, args=(zip(cosX,sinX), self.y, self.e_y, mod_abc), disp=False)
-          a = op.fmin_powell(nll, a, args=(zip(cosX,sinX), self.y, self.e_y, mod_abc), disp=False)
-          self.par.append(a)
-          self._a[k]= a[0]
-          self._b[k]= a[1]
-          self._off[k]= a[2:2+self.Nj]
-          self._lnMLj[k] = L
-          self._chisqr[k] = chisqr
-          self.lnML[k] = sum(L)
-          self._wtrms[k] = wtrms
-          W = [np.sum(1/(e_y**2+jit**2)) for e_y,jit in zip(self.e_y, a[-self.Nj:])]
-          self._wrms[k] = np.sqrt(np.sum(chisqr)/np.sum(W))   # a bit handwavy defined
-          print(k, self.nf, L)
+        self.a = [np.median(a0[len(a0)//2:])]*2 + list(a0)   # start guess for first frequency
+
+
+        start_time = time.time()   
+        with Pool(ncpus=self.ncpus) as thread: 
+          results = thread.map(self.partial_func,   [omega for omega in 2.*pi*self.freq])
+
+        for k in range(len(results)):
+
+          self.par.append(results[k][0])
+          self._a[k]= results[k][1]
+          self._b[k]= results[k][2]
+          self._off[k]= results[k][3]
+          self._lnMLj[k] = results[k][4]
+          self._chisqr[k] = results[k][5]
+          self.lnML[k] = results[k][6]
+          self._wtrms[k] = results[k][7]
+          self._wrms[k] = results[k][8]
+
         self.p = self.lnML
-        print()
+
+        print("--- %s CPUs done in %s seconds --- " % (self.ncpus, time.time() - start_time))     
+
 
     def _normcheck(self, norm):
         """
         Check normalization
-
         Parameters
         ----------
         norm : string
             Normalization string
-
         """
         if norm not in self.norms:
             raise(ValueError("Unknown norm: " + str(norm) + ". " + \
@@ -358,16 +369,13 @@ class Gls:
     def pnorm(self, norm="dlnL"):
         """
         Assign or modify normalization (can be done afterwards).
-
         Parameters
         ----------
         norm : string, optional
             The normalization to be used (default is 'lnL').
-
         Examples
         --------
         >>> gls.pnorm('wrms')
-
         """
         self._normcheck(norm)
         self.norm = norm
@@ -417,7 +425,8 @@ class Gls:
         p["ph"] = ph = arctan2(self._a[k], self._b[k]) / (2.*pi)
         p["T0"]  = self.tmin - ph/fbest
         p["offset"] = self._off[k]
-        p["jitter"] = self.par[k][-self.Nj:]
+        #print(len(self.par),self.Nj)
+        p["jitter"] = self.par[0][-self.Nj:]
 
         # Error estimates
         p["amp_err"] = sqrt(2./self.N) * rms
@@ -442,12 +451,10 @@ class Gls:
     def sinmod(self, t):
         """
         Calcuate best-fit sine curve.
-
         Parameters
         ----------
         t : array
             Time array at which to calculate the sine.
-
         Returns
         -------
         Sine curve : array
@@ -606,36 +613,28 @@ class Gls:
     def prob(self, Pn):
         """
         Probability of obtaining the given power.
-
         Calculate the probability to obtain a power higher than
         `Pn` from the noise, which is assumed to be Gaussian.
-
         .. note:: Normalization
           (see [ZK09]_ for further details).
-
           - `Scargle`:
           .. math::
             exp(-Pn)
-
           - `HorneBaliunas`:
           .. math::
             \\left(1 - 2 \\times \\frac{Pn}{N-1} \\right)^{(N-3)/2}
-
           - `Cumming`:
           .. math::
             \\left(1+2\\times \\frac{Pn}{N-3}\\right)^{-(N-3)/2}
-
         Parameters
         ----------
         Pn : float
             Power threshold.
-
         Returns
         -------
         Probability : float
             The probability to obtain a power equal or
             higher than the threshold from the noise.
-
         """
         self._normcheck(self.norm)
         if self.norm == "lnL": return (1.-Pn)**((self.N-3.)/2.)
@@ -648,20 +647,16 @@ class Gls:
     def probInv(self, Prob):
         """
         Calculate minimum power for given probability.
-
         This function is the inverse of `Prob(Pn)`.
         Returns the minimum power for a given probability threshold `Prob`.
-
         Parameters
         ----------
         Prob : float
             Probability threshold.
-
         Returns
         -------
         Power threshold : float
             The minimum power for the given false-alarm probability threshold.
-
         """
         self._normcheck(self.norm)
         if self.norm == "lnL": return 1.-Prob**(2./(self.N-3.))
@@ -674,28 +669,22 @@ class Gls:
     def FAP(self, Pn):
         """
         Obtain the false-alarm probability (FAP).
-
         The FAP denotes the probability that at least one out of M independent
         power values in a prescribed search band of a power spectrum computed
         from a white-noise time series is as large as or larger than the
         threshold, `Pn`. It is assessed through
-
         .. math:: FAP(Pn) = 1 - (1-Prob(P>Pn))^M \\; ,
-
         where "Prob(P>Pn)" depends on the type of periodogram and normalization
         and is calculated by using the *prob* method; *M* is the number of
         independent power values and is computed internally.
-
         Parameters
         ----------
         Pn : float
             Power threshold.
-
         Returns
         -------
         FAP : float
             False alarm probability.
-
         """
         prob = self.M * self.prob(Pn)
         if prob > 0.01:
@@ -705,19 +694,16 @@ class Gls:
     def powerLevel(self, FAPlevel):
         """
         Power threshold for FAP level.
-
         Parameters
         ----------
         FAPlevel : float or array_like
               "False Alarm Probability" threshold
-
         Returns
         -------
         Threshold : float or array
             The power threshold pertaining to a specified false-alarm
             probability (FAP). Powers exceeding this threshold have FAPs
             smaller than FAPlevel.
-
         """
         Prob = 1. - (1.-FAPlevel)**(1./self.M)
         return self.probInv(Prob)
@@ -725,30 +711,25 @@ class Gls:
     def stats(self, Pn):
         """
         Obtain basic statistics for power threshold.
-
         Parameters
         ----------
         Pn : float
             Power threshold.
-
         Returns
         -------
         Statistics : dictionary
             A dictionary containing {'Pn': *Pn*, 'Prob': *Prob(Pn)* ,
             'FAP': *FAP(Pn)*} for the specified power threshold, *Pn*.
-
         """
         return {'Pn': Pn, 'Prob': self.prob(Pn), 'FAP': self.FAP(Pn)}
 
     def toFile(self, ofile, header=True):
         """
         Write periodogram to file.
-
         Parameters
         ----------
         ofile : string
             Name of the output file.
-
         """
         with open(ofile, 'w') as f:
             if header:
@@ -850,5 +831,3 @@ if __name__ == "__main__":
 #from gplot import*
 #gplot(ml.freq,ml.p,',',g.freq,g.p)
 #g.plot()
-
-
